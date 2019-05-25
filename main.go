122ef7728ce824ef220d0275c8881eaf4ba8251c
Golangler <golangler@gmail.com>
2019-05-25 22:13:40 +0700

    ok

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+var letter = "a"
+var number uint64 = 18446744073709551615
 
 func main() {
-
-	fmt.Print("xxx")
-
+	fmt.Print("number is: ", strconv.FormatUint(number, 16), "\n", "letter is: ", "letter", "\n")
 }
